6a8f265831128ce11bc06e65de6b6656a786f391
SOTHY RUM <sothyrum98@gmail.com>
2026-09-04 18:48:43 +0700

    Modify script loading and cleanup in HomePage
    Updated script loading to append to body and added cleanup.

--- securekey/src/pages/HomePage.jsx
+++ securekey/src/pages/HomePage.jsx
@@ -22,6 +22,8 @@
 
 // export default HomePage;
 
+
+
 import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import HeroSection from '@/components/HeroSection';
@@ -29,22 +31,27 @@ import BestsellersSection from '@/components/BestsellersSection';
 
 function HomePage() {
   useEffect(() => {
-    // Load Adterra scripts dynamically
+    // Load first script
     const script1 = document.createElement('script');
     script1.src = 'https://pl31181497.profitableratecpmnetwork.com/bc/bb/ba/bcbbba9b9fe5dfa2ea029920fa1dda50.js';
     script1.async = true;
-    document.head.appendChild(script1);
+    document.body.appendChild(script1);
 
+    // Load second script
     const script2 = document.createElement('script');
     script2.src = 'https://pl31181498.profitableratecpmnetwork.com/a0/7a/89/a07a89c664a104816037084740e67715.js';
     script2.async = true;
-    document.head.appendChild(script2);
+    document.body.appendChild(script2);
 
-    // ❌ លុប Cleanup នេះចេញ
-    // return () => {
-    //   document.head.removeChild(script1);
-    //   document.head.removeChild(script2);
-    // };
+    // Cleanup function to remove scripts when component unmounts
+    return () => {
+      if (script1.parentNode) {
+        document.body.removeChild(script1);
+      }
+      if (script2.parentNode) {
+        document.body.removeChild(script2);
+      }
+    };
   }, []);
 
   return (
